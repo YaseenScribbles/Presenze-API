c7a6f8ac32d8507834ffb75df1555fbcc6dcf6cf
YaseenScribbles <yaseen0696@gmail.com>
2024-04-15 11:16:21 +0530

    Added try-catch in login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,19 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
     public function login(Request $request){
-        $request->validate([
-            'email' => 'required|email|exists:users,email',
-            'password' => 'required'
-        ]);
+
+        try {
+            //code...
+            $request->validate([
+                'email' => 'required|email|exists:users,email',
+                'password' => 'required'
+            ]);
+        } catch (\Throwable $th) {
+            //throw $th;
+            Log::error($th->getMessage());
+        }
 
         $credentials = $request->only('email', 'password');
 
         if(Auth::attempt($credentials)){
-            return response()->json(['message' => 'Login success', Auth::user()]);
+            return response()->json(['message' => 'Login success','user' => Auth::user()]);
         } else {
             return response()->json(['message' => 'Login failed'],401);
         }
